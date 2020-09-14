357df5e9958b5e21aed3e8434092ae232bfdb7f5
Jeremy Hamley <jeremyhamley@gmail.com>
2020-09-14 15:17:04 -0400

    step2

--- Leaflet-Step-2/static/js/config.js
+++ Leaflet-Step-2/static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "api key here"
+const API_KEY = "pk.eyJ1IjoiamVyZW15aGFtbGV5IiwiYSI6ImNrZXd6YzdlODA5NHYycWw2cGJsbDA1N2gifQ.8QtLbRKGDmyY5IyoLEzjYg"
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -4,7 +4,7 @@ var myMap = L.map("map", {
     zoom: 4
   });
   
-
+ 
 // Add tile layer to the map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
